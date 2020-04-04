8164b7f243b0af92aff28ce51ee8451fc8a0fbe7
Stefan Koch <uplink.coder@googlemail.com>
2020-04-04 12:26:33 +0200

    fastThreadLock.d
    Build the actual fast thread lock implementation

--- fastThreadLock.d
+++ fastThreadLock.d
@@ -1,217 +1,236 @@
-module moudle.thread_utils.fasTthreadLock;
+import core.thread;
 
-import core.sys.posix.pthread;
+enum Platform
+{
+    Unsupported,
+
+    X86,
+    X86_64,
+
+}
+
+version (linux)
+{
+    import core.sys.posix.pthread;
+}
+else
+{
+    static assert(0, "Platform unsupported");
+}
+immutable string readFence = ``;
+immutable string writeFence = ``;
+
+
+extern(C) uint GetThreadId()
+{
+    asm nothrow {
+                db 0x64,  0x48, 0x8b, 0x0c, 0x25, 0x00, 0x00, 0x00, 0x00;
+                // mov RCX, qword ptr FS:0x0; But dmds iasm cannot do that :(
+                // rcx = thread_id
+
+                shr RCX, 8;
+                mov RAX, RCX;
+    }
+}
+
+/// NOTE: I am aware how this function looks,
+/// Given the tools I have (buggy iasm) this the best
+string LockQueue(string lock, string retval = null)
+{
+    assert(__ctfe);
+    return
+        (retval ? `static assert(is(typeof(` ~ retval ~ `) == uint), "retval has to be a uint");` : ``) ~
+`
+    static assert(is(typeof(` ~ lock ~ `) == uint), "can only lock on uint");
+    {
+    enum lbl = mixin("\"L" ~ __LINE__.stringof ~ "\"");
+    pragma(msg, lbl);
+    printf("Locking Queue at  %s\n", lbl.ptr);
+    enum lockName = "` ~ lock ~ `";
+    uint* lockPtr = &` ~lock ~ `;
+    version (linux)
+    {
+        version (X86_64)
+        {
+            asm nothrow
+            {
+                push RAX;
+                push RCX;
+                push RDX;
+                db 0x64,  0x48, 0x8b, 0x0c, 0x25, 0x00, 0x00, 0x00, 0x00;
+                // mov RCX, qword ptr FS:0x0; But dmds iasm cannot do that :(
+                // rcx = thread_id
+
+                shr RCX, 8;
+
+                // the first byte is always 0 due to alignment
+            }
+            mixin(lbl ~ ":" ~ "
+            asm nothrow {
+                xor RAX, RAX; // rax = 0
+                mov RDX, [lockPtr]; // load ptr
+                lock; cmpxchg dword ptr [RDX], ECX; // xchg()
+                // xchg the lock value
+             " ~
+            ` ~ (retval ? `"mov ` ~ retval ~ `, EAX;"` : `""`) ~ `
+            ~ "je " ~ lbl ~ ";
+                // this code is eqivalent to
+                // if (lock == 0) { lock = thread_id; retval = lock; }
+                // else { retval = lock }
+                pop RDX;
+                pop RCX;
+                pop RAX;
+            }");
+        }
+        else static assert("Unsupported CPU");
+    }
+    else static assert("Unsupported OS");
+
+    }
+`;
+}
+
+string UnlockQueue(string lock)
+{
+    return "(*(&" ~ lock ~ ")) = 0;";
+}
+
+struct CopyQueueEntry
+{
+    void* dest;
+    const (void)* source;
+    ulong size;
+
+    ubyte[4] pad;
+}
+
+__gshared align(16) CopyQueueEntry[256] copyQueue;
+__gshared align(16) size_t copyQueueCount;
+__gshared align(16) size_t workDoneCount;
+__gshared align(16) uint copyQueueLocked;
+
+static immutable string __mmPause = "asm nothrow pure { rep; nop; }";
 import core.stdc.stdio;
 
-extern (C) size_t gettid();
+extern (C) void* copyLoop(void* arg)
+{
+    import core.stdc.string;
+    import core.stdc.stdio;
+    printf("starting copyLoop");
+    size_t nextCopyItem = 0;
+    for(;;)
+    {
+        assert(copyQueueCount < 192); // we are getting to close the limit
+        if (copyQueueCount > 32 && !copyQueueLocked)
+        {
+//            mixin(LockQueue("copyQueueLocked"));
+
+            // not a foreach because the optimizer can't see the limit
+            // due to lowering :/
+            const ourCopyQueueCount = copyQueueCount;
+
+            //written that way for some smart autovectorizer :)
+            for(int i = 0; i < ourCopyQueueCount; i += 4)
+            {
+                auto e1 = copyQueue[nextCopyItem++];
+                auto e2 = copyQueue[nextCopyItem++];
+                auto e3 = copyQueue[nextCopyItem++];
+                auto e4 = copyQueue[nextCopyItem++];
+
+                memcpy(e1.dest, e1.source, e1.size);
+                memcpy(e2.dest, e2.source, e2.size);
+                memcpy(e3.dest, e3.source, e3.size);
+                memcpy(e4.dest, e4.source, e4.size);
+
+                copyQueueCount -= 4;
+                workDoneCount += 4;
+            }
+            goto LsmallCopy;
+
+        }
+        mixin(__mmPause);
+        mixin(readFence);
+        if (copyQueueCount && !copyQueueLocked)
+        {
+//            mixin(LockQueue("copyQueueLocked"));
+        LsmallCopy:
+            for(int i = 0; i < copyQueueCount; i++)
+            {
+                auto e1 = copyQueue[nextCopyItem++];
+                memcpy(e1.dest, e1.source, e1.size);
+                workDoneCount++;
+            }
+        }
+    LCopyfinished:
+        mixin(writeFence);
+//        mixin(UnlockQueue("copyQueueLocked"));
+        // non critical
+        nextCopyItem = 0;
+        mixin(__mmPause);
+    }
+}
+
+void initWorkerThread()
+{
+    copyQueueLocked = 0;
+
+    pthread_t copyThread;
+    pthread_create(&copyThread, null, &copyLoop, null);
+
+
+}
+// enqueueMalloc()
+// enqueueRealloc()
+// enqueueMangle()
+// TOOD  --- PERFORMANCE ----
+// Replace LockedQueue by SmarterScheme which uses two rings
+
+/// Assumes that memory at source will valid forever!
+static string enqueueCopyString(string dest, string source, string size, string waitHandle = null)
+{
+    assert(__ctfe);
+
+    string result =
+"
+do {
+   printf(`pushing into the copy Queue\n`);
+    auto entry = CopyQueueEntry(
+        " ~ dest ~ ",
+        " ~ source ~ ",
+        " ~ size ~ "
+    );
+    printf(\"copyQueueLocked: %d, copyQueueCount: %d\n\", copyQueueLocked, copyQueueCount);
+    if ((!copyQueueLocked) && copyQueueCount < 192) {
+    printf(\"copyQueueLocked: %d going to lock copyQueue\n\", copyQueueLocked);
+
+        " ~ LockQueue("copyQueueLocked") ~ "
+        // when we reach this point the lock has been aquired.
+        printf(\"lock aquired\");
+        " ~ (waitHandle !is null ?  waitHandle ~ " = copyQueueCount;" : "") ~ "
+
+        copyQueue[copyQueueCount++] = entry;
+        " ~ UnlockQueue("copyQueueLocked") ~ "
+    }
+    else " ~ __mmPause ~ "
+    continue;
+} while (0);
+";
+
+  return result;
+}
 
 void main()
 {
-    pthread_t[200] threads;
-    while(true)
-    {
-        foreach(i, ref _; threads)
-        {
-            if(pthread_create(&threads[i], null, &thread_cb, cast(void*)(i + 1))) {
-                fprintf(stderr, "Error creating thread\n");
-            }
-        }
-
-        uint ctr;
-        void* retval;
-
-        foreach(i, ref _; threads)
-        {
-            pthread_join(threads[i], &retval);
-            if(retval == null) {
-                fprintf(stderr, "Well If guess we fucked up\n");
-                assert(0);
-            }
-            ctr += *(cast(uint*) &retval);
-        }
-        printf("tid retrival took %f ticks per thread\n", ctr / cast(float)threads.length);
-    }
-}
-
-void lock()
-{
-}
-
-extern (C) void* thread_cb(void* pData)
-{
-    uint tsc_begin;
-    uint tsc_end;
-//    lock();
-    asm { rdtsc; mov tsc_begin, EAX; }
-//    int threadN = cast(int)pData;
-//    auto ftid = fastThreadId();
-//    auto stid  = ((pthread_self() >> 8) & uint.max);
-//    auto stid = ftid;
-//    int retval = ftid == stid;
-    //printf("ThreadN: %d fastThreadId: %p pthread_self: %p equal: %s\n",
-    //    threadN, ftid, stid, (retval ? "true".ptr : "false".ptr)
-    //);
-    asm { rdtsc; mov tsc_end, EAX; }
-    return cast(void*) tsc_end - tsc_begin;
-//    scope(exit) unlock();
-}
-
-
-extern (C) int slowThreadId()
-{
-   version (X86_64)
-        return cast(int) ((pthread_self() >> 8) & uint.max);
-    else version (X86)
-        return pthread_self();
-}
-
-extern (C) int fastThreadId()
-{
-    version (linux)
-    {
-        version (X86)
-        {
-            asm
-            {
-                naked;
-                mov EAX, dword ptr GS:0x0;
-                ret ;
-            }
-        }
-        else version (X86_64)
-        {
-            asm
-            {
-                naked;
-                db 0x64, 0x48, 0x8b, 0x04, 0x25, 0x00, 0x00, 0x00, 0x00;
-                // mov RAX, qword ptr FS:0x0; But dmds iasm cannot do that :(
-                shr RAX, 8;
-                // the first byte is always 0 due to alignment
-                ret ;
-            }
-        }
-    }
-    else static assert ("Your OS is currently unsupported");
-}
-
-/******************************************************************************
-
-    Atomic Compare and Swap
-
-    Used to set a variable to a value in a thread-safe way
-    Returns: the value found at the point of read
-             so if it equals expected the cas was sucsessful
-    Note:
-    this function is intended for debugging purposes only
-
-******************************************************************************/
-
-public extern (C) int cas (int* val, int expected, int desired)
-{
-    version (X86_64)
-    {
-        asm
-        {
-            // RDI is val_ptr
-            // ESI is expected
-            // EDX is desired
-            naked;
-            mov EAX, ESI;
-            lock; cmpxchg dword ptr [RDI], EDX;
-            ret ;
-        }
-    }
-    else version (X86)
-    {
-        // TODO for some reason dmd1 -m32 bloats this function with nops
-        asm
-        {
-            naked ;
-            mov EDX, dword ptr [ESP+4];  // EDX is val_ptr
-            mov EAX, dword ptr [ESP+8];  // EAX is expected
-            mov ECX, dword ptr [ESP+12]; // ECX is desired
-            lock; cmpxchg dword ptr [EDX], ECX;
-            ret ;
-            db 0x0f, 0x1f, 0x00; // 3byte nop
-        }
-    }
-
-    else
-        static assert("No compare and swap for this platform");
-}
-
-unittest
-{
-    void testCas ()
-    {
-        int a = 1;
-        int b = 2;
-        int c = 3;
-        int d = 4;
-
-        assert(cas(&c, 3, 6) == 3);
-        assert(cas(&c, 3, 12) == 6);
-        assert(cas(&b, 2, 8) == 2);
-        assert(cas(&b, 8, 3) == 8);
-        assert(cas(&d, 4, ushort.max+64) == 4);
-        assert(a == 1 && b == 3 && c == 6 && d == ushort.max + 64);
-    }
-
-    testCas();
-}
-
-/******************************************************************************
-
-    Increments the value referenced by the pointer atomically
-    (syncronized among cpu-cores)
-
-    Note:
-    This function is intended for debugging purpose only
-
-******************************************************************************/
-
-public extern (C) void atomicInc (int* val)
-{
-    version (X86_64)
-    {
-        asm
-        {
-            naked;
-            lock; inc dword ptr [RDI];
-            ret ;
-        }
-    }
-    else version (X86)
-    {
-        asm
-        {
-            naked ;
-            mov EDX, dword ptr [ESP+4];
-            lock; inc dword ptr [EDX];
-            nop ; // for the ret to be aligned
-            ret ;
-            nop ; // so dmd does not emit 0x0000
-       }
-    }
-
-    else
-        static assert("No atomic increment for this platform");
-}
-
-unittest
-{
-    void testAtomicInc ()
-    {
-        int a = 1;
-        int b = 2;
-        int c = 3;
-        int d = 4;
-
-        atomicInc(&c);
-        assert(a == 1 && b == 2 && c == 4 && d == 4);
-        atomicInc(&d);
-        assert(a == 1 && b == 2 && c == 4 && d == 5);
-    }
-
-    testAtomicInc();
+    assert(!copyQueueLocked);
+    foreach(_; 0 .. 2) printf("GetThreadId: %d\n", GetThreadId());
+    // initWorkerThread();
+//    pragma(msg, LockQueue("copyQueueLocked", "owner"));
+    uint owner;
+    printf("copyQueueLocked: %d\n", copyQueueLocked);
+    mixin(LockQueue("copyQueueLocked", "owner"));
+    printf("My thread Id:%d\n", owner);
+    assert(copyQueueLocked);
+    long source = 22;
+    long dest = 0;
+    size_t wait_handle;
 }
